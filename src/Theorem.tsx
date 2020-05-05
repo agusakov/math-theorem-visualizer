8f27c0dc73517f957b6c80c5f4a50bc99c2f9a4d
Alena Gusakov <agusakov@udel.edu>
2020-05-04 22:55:17 -0400

    oh what a nightmare

--- src/Theorem.tsx
+++ src/Theorem.tsx
@@ -3,36 +3,27 @@ import { Component } from 'react';
 import firebase from "./firebase";
 import Card from '@material-ui/core/Card';
 
-interface FormProps {
-  fbkey?: string,
+interface TheoremProps {
+  fbKey: string,
 }
 
-interface FormState {
+interface TheoremState {
   fbKey: string,
   name: string,
   statement: string,
-  dependencies?: string
+  dependencies?: object
 }
 
 
-class Theorem extends Component<FormProps, FormState> {
-    /*constructor(props: FormProps) {
-        super(props);
-      }*/
+class Theorem extends Component<TheoremProps, TheoremState> {
 
       componentDidMount() {
-          firebase.database().ref(`Theorems/${this.props.fbkey}`).once('value').then( (snap) => {
+          firebase.database().ref(`Theorems/${this.props.fbKey}`).once('value').then( (snap) => {
           let dependencies = snap.val().dependencies;
           let name = snap.val().name;
           let statement = snap.val().statement;
-          /*Object.keys(snap.val()).forEach((key)=>{
-            if(key.substring(0,4)==="lead"){
-              faculty[key] = snap.val()[key].split(",");
-            }
-          });*/
-          //console.log(faculty);
           this.setState({
-            //data: snap.val(),
+            fbKey: this.props.fbKey,
             name: name,
             statement: statement,
             dependencies: dependencies
@@ -45,20 +36,30 @@ class Theorem extends Component<FormProps, FormState> {
     render() {
       return (
         <Card>
-        <div className='theorem'>
-          <header>
-              <div className='wrapper'>
-                <h1>{this.state.name}</h1>
-                
-              </div>
-          </header>
-          <div className='about'>
-            <ul>
-                <item>{this.state.statement}</item>
-                <item>{this.state.dependencies[0]}</item>
-            </ul>
+          {(this.state)?
+            <div className='theorem'>
+            <header>
+                <div className='wrapper'>
+                  <h1>{this.state.name}</h1>
+                  
+                </div>
+            </header>
+            <div className='about'>
+              <p>{this.state.statement}</p>
+              {(this.state.dependencies)
+              //<ul>
+              ?  (Object.keys(this.state.dependencies).map((key:string)=> {
+                  return <div>{key}</div>
+                }
+                ))
+              //</ul>
+              :
+              ""}
+            </div>
           </div>
-        </div>
+          :
+          <h1>loading...</h1>
+          }
         </Card>
       );
     }
